ec01d5ffc69c9223dea4cdff9bef7d3fd5ace384
DevMohammed7 <m4.st.almawali@gmail.com>
2026-04-14 11:54:52 +0400

    Service Layer Added

---
+++ LeaveMnagementSystem.web/Models/LeaveTypes/LeaveTypeDeleteVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveMnagementSystem.web.Models.LeaveTypes
+{
+    public class LeaveTypeDeleteVM : BaseLeaveTypeVM
+    {
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "NO. Days")]
+
+        public int NumberOfDays { get; set; }
+    }
+}
